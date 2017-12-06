a7b76c9186f7eda1b3360370ca04b913095d5a3a
ben harrington <harringtonben@icloud.com>
2017-12-06 17:59:30 -0600

    finished getting data from firebase. ABOUT TO SMASH THAT SHIT

--- javascripts/controllers/ProfileCtrl.js
+++ javascripts/controllers/ProfileCtrl.js
@@ -1,5 +1,23 @@
 'use strict';
 
-app.controller("ProfileCtrl", function($scope) {
-    $scope.myProfile = "This is my profile page";
+app.controller("ProfileCtrl", function($scope, AuthService, ProfileService) {
+    const getInventory = () => {
+        let inventory = [];
+        let beerlist = [];
+        let myInventory = [];
+        ProfileService.getMyInventory(AuthService.getCurrentUid()).then((results) => {
+            inventory = results;
+            ProfileService.getMyBeers().then((results) => {
+                beerlist = results;
+                console.log("My Inventory", inventory);
+                console.log("My beer list", beerlist);
+            }).catch((error) => {
+                console.log("Error in getMyBeers", error);
+            });
+        }).catch((error) => {
+            console.log("error in getInventory", error);
+        });
+    };
+
+    getInventory();
 });
---
+++ javascripts/services/ProfileService.js
@@ -0,0 +1,41 @@
+'use strict';
+
+app.service("ProfileService", function($http, $q, FIREBASE_CONFIG) {
+    const getMyInventory = (userId) => {
+        let myInventory = [];
+        return $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/inventory.json?orderBy="uid"&equalTo="${userId}"`).then((results) => {
+               let inventory = results.data;
+               if (inventory != null) {
+                Object.keys(inventory).forEach((key) => {
+                    inventory[key].id = key;
+                    myInventory.push(inventory[key]);   
+                });    
+                } 
+                resolve(myInventory);
+            }).catch((error) => {
+                reject(error);
+            });
+        });
+    };
+
+    const getMyBeers = (userId) => {
+        let myBeers = [];
+        return $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/beers.json`).then((results) => {
+               let beers = results.data;
+               if (beers != null) {
+                Object.keys(beers).forEach((key) => {
+                    beers[key].id = key;
+                    myBeers.push(beers[key]);   
+                });    
+                } 
+                resolve(myBeers);
+            }).catch((error) => {
+                reject(error);
+            });
+        });
+    };
+
+    return {getMyBeers, getMyInventory};
+});
