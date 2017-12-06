4e60f65de147ef2e370bab091a12abbb5346456a
ben harrington <harringtonben@icloud.com>
2017-12-05 20:30:56 -0600

    logic for checking against current beers in firebase is working

--- javascripts/controllers/CellarCtrl.js
+++ javascripts/controllers/CellarCtrl.js
@@ -14,11 +14,22 @@ app.controller("CellarCtrl", function($scope, CellarService, UntappdService) {
     };
 
     $scope.saveToCellar = (beer) => {
-        let beerToSave = CellarService.createBeerObject(beer);
-        CellarService.addToCellar(beerToSave).then((results) => {
-            console.log(results);
+        CellarService.searchForBeer(beer.beer.bid).then((results) => {
+            let beerToCheck = results[0];
+            console.log(beer.beer.bid);
+            console.log(beerToCheck.untappd_bid); 
+            if (beer.beer.bid === JSON.parse(beerToCheck.untappd_bid)) {
+                console.log("this beer is already saved!!");
+            } 
         }).catch((error) => {
-            console.log("Error in saveToCellar", error);
+            console.log("Error in searchForBeer", error);
         });
+
+        // let beerToSave = CellarService.createBeerObject(beer);
+        // CellarService.addToCellar(beerToSave).then((results) => {
+        //     console.log(results);
+        // }).catch((error) => {
+        //     console.log("Error in saveToCellar", error);
+        // });
     };
 });
--- javascripts/services/CellarService.js
+++ javascripts/services/CellarService.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.service("CellarService", function($http, FIREBASE_CONFIG) {
+app.service("CellarService", function($http, $q, FIREBASE_CONFIG) {
     const createBeerObject = (beer) => {
         return {
             "beer_name": beer.beer.beer_name,
@@ -19,5 +19,23 @@ app.service("CellarService", function($http, FIREBASE_CONFIG) {
         return $http.post(`${FIREBASE_CONFIG.databaseURL}/beers.json`, JSON.stringify(beer));
     };
 
-    return {addToCellar, createBeerObject};
+    const searchForBeer = (beerId) => {
+        let beerData = [];
+        return $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/beers.json?orderBy="untappd_bid"&equalTo="${beerId}"`).then((results) => {
+                let beer = results.data;
+                if (beer != null) {
+                    Object.keys(beer).forEach((key) => {
+                        beer[key].id = key;
+                        beerData.push(beer[key]);
+                    });    
+                }
+                resolve(beerData); 
+            }).catch((error) => {
+                resolve(error);
+            });
+        });
+    };
+
+    return {addToCellar, createBeerObject, searchForBeer};
 });
