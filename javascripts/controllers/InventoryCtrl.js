ad99158121762d03329246e415bcc4732cf23f26
ben harrington <harringtonben@icloud.com>
2017-12-06 20:03:38 -0600

    finished form bullshit

--- javascripts/controllers/InventoryCtrl.js
+++ javascripts/controllers/InventoryCtrl.js
@@ -9,5 +9,10 @@ app.controller("InventoryCtrl", function($routeParams, $scope, InventoryService)
         });
     };
 
+    $scope.updateInventory = (details) => {
+        console.log($scope.updateInventoryForm.for_trade, typeof $scope.updateInventoryForm.for_trade);
+    };
+   
+
     getInventoryDetail();
 });
