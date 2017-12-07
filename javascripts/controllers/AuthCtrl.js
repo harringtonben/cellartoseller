ef691999727f3428b55bb412503e3984427bcb4d
ben harrington <harringtonben@icloud.com>
2017-12-07 17:56:32 -0600

    working through auth logic

--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -4,6 +4,16 @@ app.controller("AuthCtrl", function($location, $rootScope, $scope, AuthService){
   $scope.authenticate = () => {
     AuthService.authenticateGoogle().then((result) =>{
       $rootScope.navbar = true;
+      AuthService.searchUsers().then((results) => {
+        if (isEmpty(results.data) === true) {
+          let newUser = AuthService.createUserObject(result);
+          console.log(newUser);
+          // AuthService.addUser(newUser);
+        }
+      }).catch((error) => {
+        console.log("Error in getting users", error);
+      });
+      console.log(result);
       $scope.$apply(() =>{
         $location.path("/search");
       });
@@ -11,4 +21,12 @@ app.controller("AuthCtrl", function($location, $rootScope, $scope, AuthService){
       console.log("error in authenticateGoogle", err);
     });
   };
+
+  const isEmpty = (obj) => {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+    return JSON.stringify(obj) === JSON.stringify({});
+};
 });
--- javascripts/services/AuthService.js
+++ javascripts/services/AuthService.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.service("AuthService", function ($window, FIREBASE_CONFIG) {
+app.service("AuthService", function ($http, $window, FIREBASE_CONFIG) {
   const authenticateGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     return firebase.auth().signInWithPopup(provider);
@@ -23,5 +23,22 @@ app.service("AuthService", function ($window, FIREBASE_CONFIG) {
     return false;
   };
 
-  return { authenticateGoogle, isAuthenticated, logout, getCurrentUid };
+  const searchUsers = () => {
+    return $http.get(`${FIREBASE_CONFIG.databaseURL}/users.json?orderBy="uid"&equalTo="${getCurrentUid()}"`);
+  };
+
+  const addUser = () => {
+
+  };
+
+  const createUserObject = (user) => {
+    return {
+      "email": user.additionalUserInfo.profile.email,
+      "name": user.additionalUserInfo.profile.name,
+      "picture": user.additionalUserInfo.profile.picture,
+      "uid": user.user.uid
+    };
+  };
+
+  return {authenticateGoogle, isAuthenticated, logout, getCurrentUid, searchUsers, createUserObject};
 });
