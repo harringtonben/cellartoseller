ee84c8340b0b05a0abc9a68728c61471146c9a85
ben harrington <harringtonben@icloud.com>
2017-12-11 20:42:37 -0600

    finished printing beers to page

--- javascripts/controllers/ProfileCtrl.js
+++ javascripts/controllers/ProfileCtrl.js
@@ -29,8 +29,41 @@ app.controller("ProfileCtrl", function($location, $scope, AuthService, ProfileSe
         });
     };
 
-    getInventory();
+    const getUserDetails = (tradeData) => {
+        ProfileService.getAllUsers().then((results) => {
+            tradeData.forEach((trade) => {
+                results.forEach((result) => {
+                    if (trade.owner_id === result.uid) {
+                        trade.owner_name = result.name;
+                    } else if (trade.receiver_id === result.uid) {
+                        trade.receiver_name = result.name;
+                    }
+                });
+            });
+            $scope.trades = tradeData;
+        }).catch((error) => {
+            console.log("error in getUserDetails", error);
+        });
+    };
 
+    const getTrades = () => {
+        ProfileService.getCurrentTrades().then((results) => {
+            let userId = AuthService.getCurrentUid();
+            let activeTrades = [];
+            results.forEach((result) => {
+                if (result.owner_id === userId || result.receiver_id === userId) {
+                    activeTrades.push(result);
+                }
+            });
+            let userDetails = getUserDetails(activeTrades);
+        }).catch((error) => {
+            console.log("error in getCurrentTrades");
+        });
+    };
+
+    getInventory();
+    getTrades();
+    
     $scope.updateInventory = (itemId) => {
         $location.path(`/inventory/${itemId}`);
     };
